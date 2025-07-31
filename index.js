a6eb48256e548f8801e9866c0cb656996d5d014f
unknown <tlmuunulzii@gmail.com>
2025-07-31 13:38:35 +0800

    car model

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-﻿console.log('Hello, world!');
+﻿
+let car = {
+    brand: 'Toyota',
+    model: 'Supra',
+    year: 2012
+};
+
+if (car.year < 2009
+
+){
+console.log(`${car.model} is old`);
+}else{
+    console.log(`${car.model} is new`);
+}
+
